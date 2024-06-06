b7cad2d0135f6991e917ce025c03701d25ea536b
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-06-06 08:55:46 +0300

    Initial commit

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
-MIT License
+# MIT License
 
-Copyright (c) 2024 javier-gracia-tabuenca-tuni
+Copyright (c) 2024 kanta_lab_harmonisation_public authors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ dev/01_init.R
@@ -0,0 +1,22 @@
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## activate renv and install usethis
+renv::init()
+renv::install("usethis")
+renv::install("devtools")
+
+usethis::use_mit_license()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_readme_rmd( open = FALSE )
+#usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+
+## Use git ----
+usethis::use_git()
+usethis::use_github(private = FALSE)
+
