5cdc4e54e4e128576d6990c978fbaecc5256bd02
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2026-01-26 08:21:03 +0200

    updated LABfi

--- SCRIPTS/tmpLab.R
+++ SCRIPTS/tmpLab.R
@@ -10,7 +10,7 @@ validationsSummary <- ROMOPMappingTools::validateUsagiFile(
     pathToUsagiFile,
     connection,
     vocabularyDatabaseSchema,
-    pathToValidatedUsagiFile,
+    pathToUsagiFile,
     sourceConceptIdOffset,
     pathToValidUnitsFile,
     pathToUnitConversionFile,
